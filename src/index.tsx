4f43798a021b4af7ea4c71d4e044f61e273397b6
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-01-04 18:09:31 +0200

    refactor: start to migrate from JS to TS

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App/App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        {/* <BrowserRouter basename="/goit-react-hw-08-phonebook"> */}
+        <BrowserRouter>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+}
